0a4232c0ad2c2d898b43d6523ce8f1c1639f6d20
Jignesh zala <jigneshkumar1494@gmail.com>
2021-11-09 18:00:49 +0530

    store login register response on redux

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -5,6 +5,8 @@ import { Button } from "antd";
 import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { createOrUpdateUser } from "../../functions/auth";
+
 
 const Login = ({ history }) => {
   const [email, setEmail] = useState("");
@@ -30,13 +32,21 @@ const Login = ({ history }) => {
       const { user } = result;
       const idTokenResult = await user.getIdTokenResult();
 
-      dispatch({
-        type: "LOGGED_IN_USER",
-        payload: {
-          email: user.email,
-          token: idTokenResult.token,
-        },
-      });
+      createOrUpdateUser(idTokenResult.token)
+        .then((res) => {
+          console.log("CREATE/UPDATE", res);
+          dispatch({
+            type: "LOGGED_IN_USER",
+            payload: {
+              name: res.data.name,
+              email: res.data.email,
+              token: idTokenResult.token,
+              role: res.data.role,
+              id: res.data._id,
+            },
+          });
+        })
+        .catch();
 
       history.push("/");
     } catch (error) {
@@ -52,13 +62,21 @@ const Login = ({ history }) => {
       .then(async (result) => {
         const { user } = result;
         const idTokenResult = await user.getIdTokenResult();
-        dispatch({
-          type: "LOGGED_IN_USER",
-          payload: {
-            email: user.email,
-            token: idTokenResult.token,
-          },
-        });
+        createOrUpdateUser(idTokenResult.token)
+          .then((res) => {
+            console.log("CREATE/UPDATE", res);
+            dispatch({
+              type: "LOGGED_IN_USER",
+              payload: {
+                name: res.data.name,
+                email: res.data.email,
+                token: idTokenResult.token,
+                role: res.data.role,
+                id: res.data._id,
+              },
+            });
+          })
+          .catch();
 
         history.push("/");
       })
--- src/pages/auth/RegisterComplete.js
+++ src/pages/auth/RegisterComplete.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { auth } from "../../firebase";
 import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { createOrUpdateUser } from "../../functions/auth";
 
 const RegisterComplete = ({ history }) => {
   //props.history
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  let dispatch = useDispatch();
 
   useEffect(() => {
     setEmail(window.localStorage.getItem("emailForRegistration"));
@@ -45,7 +51,23 @@ const RegisterComplete = ({ history }) => {
         const idTokenResult = await user.getIdTokenResult();
 
         console.log("user", user, "token", idTokenResult);
-        // redux store
+
+        createOrUpdateUser(idTokenResult.token)
+          .then((res) => {
+            console.log("CREATE/UPDATE", res);
+            // redux store
+            dispatch({
+              type: "LOGGED_IN_USER",
+              payload: {
+                name: res.data.name,
+                email: res.data.email,
+                token: idTokenResult.token,
+                role: res.data.role,
+                id: res.data._id,
+              },
+            });
+          })
+          .catch();
 
         // redirect
         history.push("/");
